5c40516c3d007b810673c9711f14426f537d1abd
14 <60822531+xivparagas@users.noreply.github.com>
2026-09-02 08:50:48 -0400

    Update

--- Other/SQL/update_stdcost_preview.sql
+++ Other/SQL/update_stdcost_preview.sql
@@ -3,8 +3,14 @@ GO
 
 SELECT [item_no], [avg_cost], [last_cost], [std_cost] AS current_std_cost,
     (CASE WHEN [avg_cost] >= [last_cost] THEN [avg_cost] ELSE [last_cost] END) * 1.25 AS new_std_cost,
-    ((CASE WHEN [avg_cost] >= [last_cost] THEN [avg_cost] ELSE [last_cost] END) * 1.25) - [std_cost] AS difference
+    ((CASE WHEN [avg_cost] >= [last_cost] THEN [avg_cost] ELSE [last_cost] END) * 1.25) - [std_cost] AS difference,
+    ROUND(
+        (((CASE WHEN [avg_cost] >= [last_cost] THEN [avg_cost] ELSE [last_cost] END) * 1.25) - [std_cost])
+        / NULLIF([std_cost], 0)
+    , 2) AS pct_change,
+    (CASE WHEN [avg_cost] >= [last_cost] THEN 'Average Cost' ELSE 'Last Cost' END) AS cost_basis_used
 FROM [dbo].[iminvloc_sql]
-WHERE [item_no] LIKE '301%'
+WHERE ([item_no] LIKE '021%'
+    OR [item_no] LIKE '022%')
 ORDER BY [item_no]
 GO
--- Other/SQL/update_stdcost_push.sql
+++ Other/SQL/update_stdcost_push.sql
@@ -3,5 +3,12 @@ GO
 
 UPDATE [dbo].[iminvloc_sql]
 SET [std_cost] = (CASE WHEN [avg_cost] >= [last_cost] THEN [avg_cost] ELSE [last_cost] END) * 1.25
-WHERE [item_no] LIKE '301%'
+WHERE ([item_no] LIKE '301%'
+    OR [item_no] LIKE '302%'
+    OR [item_no] LIKE '261%'
+    OR [item_no] LIKE '262%'
+    OR [item_no] LIKE '401%'
+    OR [item_no] LIKE '402%'
+    OR [item_no] LIKE '421%'
+    OR [item_no] LIKE '422%')
 GO
